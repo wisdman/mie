be675700f4fc82785d8bb59869402013516ee9a0
Yulia Fedyakina <iulia.fedyakina@gmail.com>
2020-07-14 18:07:23 +0500

    Правки в слайдах

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const detailTemplates = [
 (content) => html`
 		<div class="container detail-container golden-grid">
 			<h2 style="grid-area: 4 / 4 / span 2 / span 10;">${content.title}</h2>
-			<h3 style="grid-area: 7 / 4 / span 1 / span 10; align-self: end;" class="subtitle">${content.subtitle}</h3>
+			<h3 style="grid-area: 5 / 4 / span 1 / span 10; align-self: end;" class="subtitle">${content.subtitle}</h3>
 			<div class="detail-video-container" style="grid-area: 7 / 1 / span 20 / span 20;">
 				
 				<video src="${content.image}" type="video/mp4" autoplay muted loop style="margin-left: -300px;"></video>
@@ -52,7 +52,7 @@ const detailTemplates = [
 (content) => html`
 		<div class="container detail-container golden-grid">
 			<h2 style="grid-area: 4 / 4 / span 2 / span 10;">${content.title}</h2>
-			<h3 style="grid-area: 7 / 4 / span 1 / span 10; align-self: end;" class="subtitle">${content.subtitle}</h3>
+			<h3 style="grid-area: 5 / 4 / span 1 / span 10; align-self: end;" class="subtitle">${content.subtitle}</h3>
 			<div class="detail-video-container" style="grid-area: 8 / 1 / span 20 / span 15;">
 				
 				<video src="${content.image}" type="video/mp4" autoplay muted loop style="margin-left: -200px;"></video>
@@ -66,7 +66,7 @@ const detailTemplates = [
 (content) => html`
 		<div class="container detail-container golden-grid">
 			<h2 style="grid-area: 4 / 4 / span 2 / span 8;">${content.title}</h2>
-			<h3 style="grid-area: 7 / 4 / span 1 / span 8; align-self: end;" class="subtitle">${content.subtitle}</h3>
+			<h3 style="grid-area: 5 / 4 / span 1 / span 8; align-self: end;" class="subtitle">${content.subtitle}</h3>
 			<div class="detail-video-container" style="grid-area: 9 / 1 / span 10 / span 13;">
 				
 				<video src="${content.image}" type="video/mp4" autoplay muted loop style="margin-left: -70px;"></video>
@@ -81,7 +81,7 @@ const detailTemplates = [
 (content) => html`
 		<div class="container detail-container golden-grid">
 			<h2 style="grid-area: 4 / 4 / span 2 / span 10;">${content.title}</h2>
-			<h3 style="grid-area: 6 / 4 / span 1 / span 10; align-self: end;" class="subtitle">${content.subtitle}</h3>
+			<h3 style="grid-area: 5 / 4 / span 1 / span 10; align-self: end;" class="subtitle">${content.subtitle}</h3>
 			<div class="detail-video-container" style="grid-area: 7 / 1 / span 20 / span 20;">
 				
 				<video src="${content.image}" type="video/mp4" autoplay muted loop style="margin-left: -300px;"></video>
@@ -95,7 +95,7 @@ const detailTemplates = [
 (content) => html`
 		<div class="container detail-container golden-grid">
 			<h2 style="grid-area: 4 / 4 / span 2 / span 8;">${content.title}</h2>
-			<h3 style="grid-area: 7 / 4 / span 1 / span 10; align-self: end;" class="subtitle">${content.subtitle}</h3>
+			<h3 style="grid-area: 5 / 4 / span 1 / span 10; align-self: end; transform: translateY(140px);" class="subtitle">${content.subtitle}</h3>
 			<div class="detail-video-container" style="grid-area: 7 / 1 / span 20 / span 15;">
 				
 				<video src="${content.image}" type="video/mp4" autoplay muted loop style="margin-left: -200px;"></video>
@@ -110,7 +110,7 @@ const detailTemplates = [
 (content) => html`
 		<div class="container detail-container golden-grid">
 			<h2 style="grid-area: 4 / 4 / span 2 / span 8;">${content.title}</h2>
-			<h3 style="grid-area: 7 / 4 / span 1 / span 10; align-self: end;" class="subtitle">${content.subtitle}</h3>
+			<h3 style="grid-area: 5 / 4 / span 1 / span 10; align-self: end; transform: translateY(140px);" class="subtitle">${content.subtitle}</h3>
 			<div class="detail-video-container" style="grid-area: 8 / 1 / span 20 / span 15;">
 				
 				<video src="${content.image}" type="video/mp4" autoplay muted loop style="margin-left: -200px;"></video>
@@ -125,7 +125,7 @@ const detailTemplates = [
 (content) => html`
 		<div class="container detail-container golden-grid">
 			<h2 style="grid-area: 4 / 4 / span 2 / span 8;">${unsafeHTML(content.title)}</h2>
-			<h3 style="grid-area: 7 / 4 / span 1 / span 10; align-self: end;" class="subtitle">${content.subtitle}</h3>
+			<h3 style="grid-area: 5 / 4 / span 1 / span 10; align-self: end; transform: translateY(140px);" class="subtitle">${content.subtitle}</h3>
 			<div class="detail-video-container" style="grid-area: 8 / 1 / span 20 / span 15;">
 				
 				<video src="${content.image}" type="video/mp4" autoplay muted loop style="margin-left: -200px;"></video>
@@ -155,7 +155,7 @@ const detailTemplates = [
 (content) => html`
 		<div class="container detail-container golden-grid">
 			<h2 style="grid-area: 4 / 4 / span 2 / span 15;">${content.title}</h2>
-			<h3 style="grid-area: 6 / 4 / span 1 / span 8; align-self: end;" class="subtitle">${unsafeHTML(content.subtitle)}</h3>
+			<h3 style="grid-area: 5 / 4 / span 1 / span 8; align-self: end;" class="subtitle">${unsafeHTML(content.subtitle)}</h3>
 			<div class="detail-video-container" style="grid-area: 8 / 1 / span 20 / span 17;">
 				
 				<video src="${content.image}" type="video/mp4" autoplay muted loop style="margin-left: -250px;"></video>
@@ -169,7 +169,7 @@ const detailTemplates = [
 (content) => html`
 		<div class="container detail-container golden-grid">
 			<h2 style="grid-area: 4 / 4 / span 2 / span 10;">${content.title}</h2>
-			<h3 style="grid-area: 6 / 4 / span 1 / span 10; align-self: end;" class="subtitle">${content.subtitle}</h3>
+			<h3 style="grid-area: 5 / 4 / span 1 / span 10; align-self: end;" class="subtitle">${content.subtitle}</h3>
 			<div class="detail-video-container" style="grid-area: 6 / 1 / span 20 / span 20;">
 				
 				<video src="${content.image}" type="video/mp4" autoplay muted loop style="margin-left: -400px;"></video>
@@ -183,7 +183,7 @@ const detailTemplates = [
 	const detailTemplate2 = (content) => html`
 		<div class="container detail-container golden-grid">
 			<h2 style="grid-area: 4 / 4 / span 2 / span 10;">${content.title}</h2>
-			<h3 style="grid-area: 6 / 4 / span 1 / span 10; align-self: end;" class="subtitle">${content.subtitle}</h3>
+			<h3 style="grid-area: 5 / 4 / span 1 / span 10; align-self: end;" class="subtitle">${content.subtitle}</h3>
 			<div class="detail-video-container" style="grid-area: 6 / 1 / span 20 / span 20;">
 				
 				<video src="/video/Plotinka_Oct_04.webm" type="video/mp4" autoplay muted loop style="margin-left: -300px;"></video>
